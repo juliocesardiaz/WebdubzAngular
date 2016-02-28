3e4654c453c59bd64c0250fa2d764b05979c190b
Julio <jcdiaz92@Julios-MacBook-Pro.local>
2016-02-27 22:11:28 -0800

    adds api calls and starting to get the waveforms drawn

--- controllers/TraxController.js
+++ controllers/TraxController.js
@@ -1,9 +1,11 @@
+/* global angular */
 angular.module('webdubz')
-   .controller('TraxCtrl', ['$scope', 'TraxFactory', 
-      function($scope, TraxFactory) {
+   .controller('TraxCtrl', ['$scope', 'TraxFactory', function($scope, TraxFactory) {
    $scope.status;
    $scope.trax;
+   
    getTrax();
+   
    function getTrax() {
         TraxFactory.getTrax()
             .success(function (traxx) {
@@ -12,5 +14,21 @@ angular.module('webdubz')
             .error(function (error) {
                 $scope.status = 'Unable to load customer data: ' + error.message;
             });
-    };     
+    };  
+    $scope.wavesurfer = Object.create(WaveSurfer);
+     
+    $scope.loadTrax = function(track) {
+      
+      console.log(track.id)
+      $scope.wavesurfer.init({
+         container: '#' + track.artist,
+         waveColor: 'violet',
+         progressColor: 'purple'
+      });
+      $scope.wavesurfer.load(track.path_lq);
+      $scope.wavesurfer.play();
+    };
+    $scope.playTrax = function() {
+       
+    };
 }]);
--- services/TraxFactory.js
+++ services/TraxFactory.js
@@ -1,8 +1,11 @@
 angular.module('webdubz')
    .factory('TraxFactory', ['$http', function($http) {
+      
       var TraxFactory = {};
+      
       TraxFactory.getTrax = function() {
          return $http.get('http://localhost:8000/api/v1/dubz');
       };
+      
       return TraxFactory;
 }]);
